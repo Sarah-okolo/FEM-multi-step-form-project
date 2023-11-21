3f10c69484c01def77ab3c81d1c3303241e069a1
Sarah-okolo <okolosarah402@gmail.com>
2023-11-21 15:11:25 +0100

    fixed background image path of nav-sidebar

--- src/components/nav-sidebar.jsx
+++ src/components/nav-sidebar.jsx
@@ -4,8 +4,28 @@ function NavSidebar() {
   return ( 
     <>
       <nav id="nav-sidebar">
-        <div>
+        <div id='nav-links-container'>
           
+          <div className='link-wrapper'>
+           <div className="round-nav-btn">1</div>
+            <p className="what-section"><span className='step-faded-txt'>Step 1</span><br/>YOUR INFO</p>
+          </div>
+        
+          <div className='link-wrapper'>
+           <div className="round-nav-btn">2</div>
+            <p className="what-section"><span className='step-faded-txt'>Step 2</span><br/>SELECT PLAN</p>
+          </div>
+
+          <div className='link-wrapper'>
+            <div className="round-nav-btn">3</div>
+            <p className="what-section"><span className='step-faded-txt'>Step 3</span><br/>ADD-ONS</p>
+          </div>
+         
+          <div className='link-wrapper'>
+            <div className="round-nav-btn">4</div>
+            <p className="what-section"><span className='step-faded-txt'>Step 4</span><br/>SUMMARY</p>
+          </div>
+            
         </div>
       </nav>
     </>
