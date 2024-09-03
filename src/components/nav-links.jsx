cb935dfb20ba2c6351c99bd79ff5b68fdb8fd3cf
Sarah-okolo <okolosarah402@gmail.com>
2024-09-03 01:19:32 +0100

    Adjusted mobile responsiveness

--- src/components/nav-links.jsx
+++ src/components/nav-links.jsx
@@ -23,17 +23,25 @@ function NavLinks(props) {
 
   // removes the active state of all section btns and sets only for the currently clicked link btn.
   function setActive() {
-    document.querySelectorAll(".round-nav-btn").forEach((link) => link.classList.remove('active'));
-    linksRef.current.classList.add('active');
+    document.querySelectorAll(".round-nav-btn").forEach((link) => {
+      link.style.backgroundColor = 'transparent'
+      link.style.color = 'hsl(229, 24%, 87%)';
+      link.style.border = '1px solid white';
+
+  });
+    linksRef.current.style.backgroundColor = 'hsl(206, 94%, 87%)';
+    linksRef.current.style.color = 'hsl(213, 96%, 18%)';
+    linksRef.current.style.border = 'none';
     setBtnLinkNumber(Number(linksRef.current.innerText))
   }
+
 
   return (
     <>
       <div className='link-wrapper'>
         <a href={destination} ref={linksRef} className="round-nav-btn" onClick={setActive}>{stepNumber}</a>
         <p className="what-section">
-          <span className='step-faded-txt'>Step {stepNumber}</span><br/>{stepName }
+          <span className='step-faded-txt'>Step {stepNumber}</span><br/>{stepName}
         </p>
       </div>
     </>
